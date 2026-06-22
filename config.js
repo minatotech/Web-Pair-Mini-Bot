086ab7ea7836542c767a705f3487be49c9eb5b6f
minatotech <minatotechx@gmail.com>
2026-06-23 03:16:10 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    DEV_NAME: "Minato",
+    PORT: process.env.PORT || 3050,
+    TARGET_NEWSLETTER: "120363426550886892@newsletter",
+    SESSIONS_DIR: "./sessions",
+    APP_TITLE: "BANKAI MULTI-PAIR",
+    SUBTITLE: "Deploy concurrent instances with distinct phone sessions"
+};
